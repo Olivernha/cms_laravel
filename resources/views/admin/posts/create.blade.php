16d7d65aa879cfb732e200a80ae24a1c5ba0cf0f
oliver <nayhtetaung201733@gmail.com>
2020-08-25 14:10:27 +0630

    Able to create Post with full fun category

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div class="form-group">
                 {!! Form::label('category_id','Category:') !!}
-                {!! Form::select('category_id',array(1=>'PHP',2=>'Java'),null,['class' =>'form-control']) !!}
+                {!! Form::select('category_id',[''=>'Choose Category']+$categories,null,['class' =>'form-control']) !!}
             </div>
             <div class="form-group">
                 {!! Form::label('photo_id','Photo') !!}
